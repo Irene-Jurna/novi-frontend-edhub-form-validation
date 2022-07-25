629cd5617fa4ba3e636090bf9db4dfdac8469e54
Irene Jurna <irene.jurna@gmail.com>
2022-07-25 18:12:49 +0200

    opdracht-klaar

--- main.js
+++ main.js
@@ -3,12 +3,36 @@ function handleInputChange(e) {
     const currentInput = e.target.name;
     console.log(`In het ${currentInput}-veld is ingevuld: ${currentValue}`);
     console.log(e.keyCode);
+
+    if (e.target.value.includes('@')) {
+        usernameWarning.textContent = 'Gebruikersnaam mag geen @ bevatten.';
+    } else {
+        usernameWarning.textContent = '';
+    }
 }
 
 const usernameInputField = document.getElementById('username-field');
-
+const usernameWarning = document.getElementById('warning-message-username');
 usernameInputField.addEventListener('keyup', handleInputChange);
 
-const passwordInputField = document.getElementById('password-field');
+function handleInputLength(e) {
+    const currentValue = e.target.value;
+    const currentInput = e.target.name;
+    console.log(`In het ${currentInput}-veld is ingevuld: ${currentValue}`);
+    console.log(e.keyCode);
 
-passwordInputField.addEventListener('keyup', handleInputChange);
+    if (e.target.value.length > 6 || e.target.value.length === 0) {
+        passwordWarning.textContent = '';
+    } else {
+        passwordWarning.textContent = 'Je wachtwoord moet langer zijn dan 6 tekens.';
+    }
+}
+
+const passwordInputField = document.getElementById('password-field');
+const passwordWarning = document.getElementById('warning-message-password');
+
+passwordInputField.addEventListener('keyup', handleInputLength);
+
+
+
+
